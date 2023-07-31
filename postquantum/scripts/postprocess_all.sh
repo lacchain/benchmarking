8458866d3f3bae03e1c6a5dc7c18f1fa8c7e0c40
eum602 <eum602@gmail.com>
2023-07-31 12:26:18 -0500

    add test results for falcon precompiled

---
+++ postquantum/scripts/postprocess_all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+HERE=$(pwd)
+cd ../rawdata
+FILES=geth*
+for f in $FILES; do
+  echo "Processing $f"
+  python3 ../scripts/postprocess_geth_v2.py $f >../processed/$f.md
+done
+
+cd $HERE
